a048a420f2fe2fb22f5d2e8b0e1893d4832139fd
shifnakn <shifnakn21@gmail.com>
2026-04-01 10:05:53 +0530

    Add files via upload

---
+++ program18/UserAuthentication.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class InvalidLoginException extends Exception{
+public InvalidLoginException(String message){
+ super(message);
+}}
+public class UserAuthentication{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+String username,password;
+System.out.println("enter username:");
+username=sc.next();
+System.out.println("enter password:");
+password=sc.next();
+try{
+if(username.equals("admin") && password.equals("123")){
+System.out.println("login successful");
+}
+else{
+throw new InvalidLoginException("invalid username and password");
+}
+}
+catch(InvalidLoginException e){
+System.out.println("Exception: " +e.getMessage());
+}}}
